ab414e991b502ee153f6a5fead18c737cbfb7e76
kjj6198 <kjj6198@gmail.com>
2020-03-16 01:12:21 +0900

    Add full text srt function

---
+++ formatter.py
@@ -0,0 +1,41 @@
+from math import floor
+
+
+def _pad(num, pad_num=2):
+    if (pad_num is 2):
+        return "{:02d}".format(int(num))
+    else:
+        return "{:03d}".format(int(num))
+
+# generate fulltext
+
+
+def format_fulltext(transcripts, outfile=""):
+    full_texts = [transcript["text"] for transcript in transcripts]
+    with open(outfile, "a") as file:
+        for t in full_texts:
+            file.write("{t}\n".format(t=t))
+
+
+def format_srt(transcripts, outfile=""):
+    with open(outfile, "a") as file:
+        for (i, t) in enumerate(transcripts):
+            # start time
+            sh = _pad(t["start_time"] // 3600)
+            sm = _pad(floor(t["start_time"] % 3600 / 60))
+            ss = _pad(floor(t["start_time"] % 60))
+            smf = _pad(floor(t["start_time"] * 1000) % 1000, pad_num=3)
+            # end time
+            eh = _pad(t["end_time"] // 3600)
+            em = _pad(floor(t["end_time"] % 3600 / 60))
+            es = _pad(floor(t["end_time"] % 60))
+            esf = _pad(floor(t["end_time"] * 1000) % 1000, pad_num=3)
+
+            file.writelines([
+                "{}\n".format(i+1),
+                "{h}:{m}:{s},{sms} --> {eh}:{em}:{es},{ems}\n".format(
+                    h=sh, m=sm, s=ss, sms=smf,
+                    eh=eh, em=em, es=es, ems=esf
+                ),
+                "{text}\n\n".format(text=t["text"])
+            ])
