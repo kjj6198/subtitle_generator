ee940898d6d265b43971cf60be34cef753d8bc17
kjj6198 <kjj6198@gmail.com>
2020-03-18 00:04:21 +0900

    replace with __init__.py

--- __init__.py
+++ __init__.py
@@ -2,11 +2,22 @@ from google_speech_recognizer import GoogleSpeechRecognizer
 from extract_region import find_speech_regions
 from audio_convert import AudioOperation
 from formatter import format_fulltext, format_srt
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Generate subtitle from video file!')
+
+parser.add_argument('infile', argparse.FileType('r'), nargs=1)
+parser.add_argument('-o', type=str, nargs=1, help="output srt name")
+parser.add_argument('-l', type=str, nargs=1, help="language")
+
+args = parser.parse_args()
 
 
 def main():
     recognizer = GoogleSpeechRecognizer(rate=44100, encoding="MP3")
-    converter = AudioOperation("1.mp4")
+    converter = AudioOperation(args.infile)
     audio_file = converter.extract_audio()
     regions = find_speech_regions(audio_file)
 
@@ -21,10 +32,11 @@ def main():
                 "end_time": end_time,
                 "text": u"{}".format(result.alternatives[0].transcript)
             })
-    format_srt(transcripts, "./text.srt")
-    format_fulltext(transcripts, "./text.txt")
+    format_srt(transcripts, args.outfile)
+    format_fulltext(transcripts, args.outfile)
     print("Full text file generated: ./text.txt")
     print("Subtitle text file generated: ./text.srt")
 
 
-main()
+if __name__ == 'main':
+    sys.exit(main())
